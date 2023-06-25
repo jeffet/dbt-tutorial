9e410261b76b7d819b6f1381bb88429d7ad16424
Jeff Tennenbaum <tennenjl@us.ibm.com>
2023-06-25 16:25:48 -0400

    added staging sql models to separate logic

--- models/customers.sql
+++ models/customers.sql
@@ -5,27 +5,15 @@
   )
 }}
 
-
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-test-390721.jaffle_shop.customers`
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-test-390721.jaffle_shop.orders`
+    select * from {{ ref('stg_orders') }}
 
 ),
 
@@ -61,3 +49,4 @@ final as (
 )
 
 select * from final
+
---
+++ models/stg_customers.sql
@@ -0,0 +1,6 @@
+select
+    id as customer_id,
+    first_name,
+    last_name
+
+from `dbt-test-390721.jaffle_shop.customers`
